64f0dac95e5ff5b7696003a90bf622f7fc5ee84e
Dana Speianu <speianu.dana@gmail.com>
2021-02-28 13:52:11 +0200

    added lab3 predicction

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
+from prediction import predict
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>This is my ml app</h1>"
+    return "<h1>Training score: </h1>" + predict()[0]
+
 
 if __name__ == '__main__':
     app.run()
---
+++ prediction.py
@@ -0,0 +1,28 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import pandas
+
+
+def predict():
+    path = 'https://raw.githubusercontent.com/speianudana/FIA/main/Lab3/apartmentComplexData.txt'
+    columns = ['1', '2', 'complex_age', 'total_rooms', 'total_bedrooms', 'complex_inhabitants', 'apartments_nr', '8',
+               'median_complex_value']
+
+    dataset = pandas.read_csv(path, names=columns)
+    dataset.describe()
+
+    dataset.corr()
+    x = dataset[
+        ['1', '2', 'complex_age', 'total_rooms', 'total_bedrooms', 'complex_inhabitants', 'apartments_nr', '8']]
+
+    y = dataset[['median_complex_value']].values.flatten()
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=70)
+
+    training = LinearRegression().fit(x_train, y_train)
+    training_score = training.score(x_train, y_train)
+    testing_score = training.score(x_test, y_test)
+    actual_price = y_test[0]
+    predicted_price = training.predict([x_test.iloc[0]])[0]
+
+    return training_score, testing_score, actual_price, predicted_price
