63d36f6ad28474498f383e8989c074bb4d526743
Dana Speianu <speianu.dana@gmail.com>
2021-02-28 18:07:39 +0200

    added prediction

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from prediction import predict
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Training score: </h1>"
+    prediction = predict()[0]
+    return "Prediction: {}"+format(prediction)
+
+
+# @app.route('/predict')
+# def hello_name():
+#     return predict()[0]
 
 
 if __name__ == '__main__':
